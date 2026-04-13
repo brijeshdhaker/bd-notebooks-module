abdecc6c2cb52dd6644c465ffbf95ffbc0c8a2e3
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2026-04-13 14:57:48 +0530

    2026-04-13 notebooks updated.

--- setup-and-cluster-submit.sh
+++ setup-and-cluster-submit.sh
@@ -15,7 +15,7 @@ python -m venv v_env
 source v_env/bin/activate
 pip cache purge
 pip install -U pip
-pip install -r requirements.txt
+pip install -U -r requirements.txt
 deactivate
 
 # Here we package up an isolated environment that we'll ship to YARN.
