eb73aa7239c8d64fbf173297973d1060c94975fd
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2026-05-24 18:36:13 +0530

    2025-05-24 Update mysql configurations

--- conf/postgres/init-user-db.sh
+++ conf/postgres/init-user-db.sh
@@ -17,13 +17,13 @@ psql -v ON_ERROR_STOP=1 --host postgresql.sandbox.net --username "$POSTGRES_USER
 	GRANT CONNECT ON DATABASE sandboxdb TO pgadmin;
 	
 	-- Grant connect schema to user
-	GRANT USAGE ON SCHEMA sandboxdb TO pgadmin;
+	GRANT USAGE ON SCHEMA public TO pgadmin;
 	
 	-- Grant read access to all existing tables
-	GRANT SELECT ON ALL TABLES IN SCHEMA sandboxdb TO pgadmin;
+	GRANT SELECT ON ALL TABLES IN SCHEMA public TO pgadmin;
 	
 	-- Grant full access to all existing tables
-	GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA sandboxdb TO pgadmin;
+	GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pgadmin;
 
 EOSQL
 
@@ -40,12 +40,12 @@ END $$;
 sudo -u postgres psql -c 'SHOW hba_file;'
 
 
-SELECT table_name 
+SELECT table_catalog, table_name, table_schema, table_type 
 FROM information_schema.tables 
 WHERE table_schema = 'public' 
 AND table_type = 'BASE TABLE';
 
-List tables in current schema: \dt.
-List tables in all schemas: \dt *.*.
+List tables in current schema: \dt
+List tables in all schemas: \dt *.*
 List with more details: \dt+ (includes size and description).
-List tables and views: \d.
+List tables and views: \d
